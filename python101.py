5027fb22b639ef94544bcbf418a52e338d908be9
sandra oduro-twumasi <sandraoduro-twumasi@Maames-iMac.local>
2020-05-08 15:53:59 -0700

    trying to loop calculator

--- python101.py
+++ python101.py
@@ -24,5 +24,67 @@
  #   print (b, "the even numbers under 20 are", b*2)
  #end
  #  adding for loop with else to a range(start,stop,step_size)
-for x in range (1,10):
-     print(x, "the odd numbers are",(x*2)-1)
+#for x in range (1,10):
+#   print(x, "the odd numbers are",(x*2)-1)
+#end
+
+
+#building a calculator application with for loops
+
+
+#defining 
+#def is function
+#decimals are floats whole numbers are integers. commas mean space
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y 
+
+def again():
+   return "Y" or "N"
+
+
+
+print("What's your operation?")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+choice = input("Enter choice(1/2/3/4):")
+
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+#operation based on user choice
+if choice == '1':
+   print(num1,"+",num2,"=", add(num1,num2))
+
+elif choice == '2':
+   print(num1,"-",num2,"=", subtract(num1,num2))
+
+elif choice == '4':
+   print(num1,"/",num2,"=", divide(num1,num2))
+
+elif choice == '3':
+   print(num1,"*",num2,"=", multiply(num1,num2))
+#relief valve
+else:
+   print("Invalid input. Do you want to try again?")
+
+def choice():
+    input("Enter choice(1/2/3/4):")
+again = input("Do you want to calculate again? Please type Y for YES or N for NO")
+if again.upper() =='Y': 
+   choice()
+elif again.upper() =='N': 
+   print("Thanks for trying")
+
+
